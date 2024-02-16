d180c1cc28f2845194176ea639781db9a68bf6b5
Micah Lessnick <micahlessnick@gmail.com>
2024-02-16 12:27:36 -0500

    Updated: interface to take in optional user vars

--- src/main/java/edu/brown/cs/student/main/Server/LocationDataFinder.java
+++ src/main/java/edu/brown/cs/student/main/Server/LocationDataFinder.java
@@ -4,6 +4,5 @@ import edu.brown.cs.student.main.BroadbandDataAPI.LocationData;
 
 public interface LocationDataFinder {
 
-    public LocationData find(String state, String county);
-
+  public LocationData find(String state, String county, String dataVars);
 }
